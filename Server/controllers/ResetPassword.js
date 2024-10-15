383817ab03a513eed16bf0c56dbb0f0ab1b22fee
Nikhil Singh <95135482+karaskaki@users.noreply.github.com>
2024-10-15 13:38:07 +0530

    Update ResetPassword.js

--- Server/controllers/ResetPassword.js
+++ Server/controllers/ResetPassword.js
@@ -32,7 +32,7 @@ exports.resetPasswordToken = async (req, res) => {
 		console.log("DETAILS", updatedDetails);
 
 		// create url
-		const url = `http://localhost:3000/update-password/${token}`;
+		const url = `https://studynotion-ejlr.onrender.com/api/v1/update-password/${token}`;
 
 		// send mail containing the url
 		await mailSender(
